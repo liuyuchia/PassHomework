ff90f832d3983b9189164a896ad8e390445e0cff
liuyuchia <ax895336@gmail.com>
2017-03-15 03:02:16 +0800

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php 
+$name = $_FILES['file']['name'];
+$type = $_FILES['file']['type'];
+$size = $_FILES['file']['size'];
+$tmp_name = $_FILES['file']['tmp_name'];
+$error = $_FILES['file']['error']; 
+//$uploads = "\uploads".$name;
+
+$target_path = "uploads/"; //指定上傳資料夾
+$dest = $target_path.$name;//上傳檔案名稱
+
+if(move_uploaded_file($tmp_name,iconv("UTF-8", "big5", $dest ))) {
+	echo "檔案：". $name ."<br/>". " 上傳成功!"."<br/>"."<br/>";
+}else{
+	echo "檔案上傳失敗，請再試一次!"."<br/>";
+}
+
+
+	echo "檔案名稱: ".$name."<br/>";
+	echo "檔案類型: " .$type."<br/>";
+	echo "檔案大小: " .($size / 1024)." Kb<br />";
+	echo "暫存名稱: " .$tmp_name."<br/>";
+	echo "錯誤: " .$error."<br/>";
+	
+
+?>   
+
